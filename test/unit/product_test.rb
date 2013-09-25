9328da98173f8a43c3f98556a01aca5404fe67f7
maxdota <sieu_ngoc_2007@yahoo.com>
2013-09-25 11:23:06 +0700

    view items in catalog

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -8,10 +8,10 @@ class ProductTest < ActiveSupport::TestCase
   test "product attributes must not be empty" do
   	product = Product.new
   	assert product.invalid?
-  	assert product.errors[:titlle].any?
-  	assert product.errors[:description].any?
-  	assert product.errors[:price].any?
-  	assert product.errors[:image_url].any?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
   end
 
   test "product price must be positive" do
@@ -20,12 +20,12 @@ class ProductTest < ActiveSupport::TestCase
                           image_url:   "zzz.jpg")
     product.price = -1
     assert product.invalid?
-    assert_equal ["must be greater than or equal to 0.01"],
+    assert_equal "must be greater than or equal to 0.01",
       product.errors[:price].join('; ')
 
     product.price = 0
     assert product.invalid?
-    assert_equal ["must be greater than or equal to 0.01"], 
+    assert_equal "must be greater than or equal to 0.01", 
       product.errors[:price].join('; ')
 
     product.price = 1
@@ -60,7 +60,7 @@ class ProductTest < ActiveSupport::TestCase
                           image_url:   "fred.gif")
 
     assert product.invalid?
-    assert_equal ["has already been taken"], product.errors[:title].join('; ')
+    assert_equal "has already been taken", product.errors[:title].join('; ')
   end
 
   test "product is not valid without a unique title - i18n" do
@@ -70,7 +70,7 @@ class ProductTest < ActiveSupport::TestCase
                           image_url:   "fred.gif")
 
     assert product.invalid?
-    assert_equal [I18n.translate('activerecord.errors.messages.taken')],
+    assert_equal I18n.translate('activerecord.errors.messages.taken'),
                  product.errors[:title].join('; ')
   end
 
