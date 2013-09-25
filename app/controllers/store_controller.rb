3f4eaeb27d5bd53c6b638cad941b826576f977f5
maxdota <sieu_ngoc_2007@yahoo.com>
2013-09-25 14:59:15 +0700

    buyer can add products to a simple cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,11 @@
 class StoreController < ApplicationController
   def index
-  	@products = Product.order(:title)
+    if session[:counter].nil?
+      session[:counter] = 1
+    else
+      session[:counter] = session[:counter] + 1
+    end
+    @counter = session[:counter]
+    @products = Product.order(:title)
   end
 end
